67d815c6d76f7b1d955e2dc354cfe78cd94b48c3
Kapil Kunwar <58800731+kapil125@users.noreply.github.com>
2020-07-16 12:13:35 +0545

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -14,19 +14,37 @@ let barValue = 0;
 let result;
 let checker = 0;
 let userAnswers = [];
+
+const songPlaying = window.confirm('Wants To Play Quiz With StrangerThings Theme Song????');
+if(songPlaying === true) {
+  play();
+} else{
+  alert('Theme Song Will Not Be Played');
+}
+
 const questions = [
   `2)What is real name of Mike Wheeler?`,
   `3)What is Max fullname?`,
   `4)Soundtrack played when Will's body is found?`,
   `5)What's Jim Hopper daughter's name?`,
+  `6)What's El's mother name?`,
+  `7)What's the name of S01 Chapter 4?`,
+  `8)Where are Billy and Max from?`,
+  `9)What is supposed to attract Demogorgan?`,
+  `10)What's Dustin name according to Mouth-Breather?`
 ];
 const answers = [
   ["Mike Wolfhard", "Finn Wolfhard", "Noah Schanapp", "Joy Keery"],
-  ["Maxene Sinclair", "Maxene Byers", "Maxene Mayfield", "Maxene Hopper"],
-  ["Night We Met", "Die In Your Arms", "Never Surrender", "Heroes"],
-  ["Sarah", "Jane", "Erica", "Millie"],
+  ["Maxine Sinclair", "Maxine Byers", "Maxine Hopper", "Maxine Mayfield"],
+  ["Heroes", "Die In Your Arms", "Never Surrender", "Night We Met"],
+  ["Millie", "Jane", "Erica", "Sarah"],
+  ["Karen", "Joyce", "Terry", "Maya"],
+  ["Jolly Wolly", "Body Of Will Byers", "The Body", "Died Will"],
+  ["Mirkwood", "Hawkins", "Paris", "California"],
+  ["Meat", "Blood", "Animals", "Human"],
+  ["Toothless", "Frog-Face", "Midnight", "Weirdo"]
 ];
-const correctAns = ["one", "two", "three", "four", "one"];
+const correctAns = ["three", "two", "four", "one", "four", "three", "three", "four", "two", "one"];
 
 const nextQuestion = () => {
   if (
@@ -61,7 +79,6 @@ const click = (check) => {
     alert("answerIsLocked");
   } else {
     check.className = "active";
-    play();
     userAnswers.push(check.id);
     if (checker === questions.length) {
       resultBox.innerHTML = `
@@ -82,7 +99,6 @@ const totalScored = () => {
   barBox.style.display = "flex";
 
   let scored = scoreCalculation();
-
   const progressBar = () => {
     const barProg = document.getElementById("inner-bar");
     if(barValue >= scored){
@@ -101,11 +117,12 @@ const scoreCalculation  = () => {
   let totalScore = 0;
   for(i = 0; i< userAnswers.length; i++) {
     if(correctAns[i] === userAnswers[i]) {
-      totalScore += 20;
+      totalScore += 10;
     }
   }
   return totalScore;
 };
+
 
 nextBtn.addEventListener("click", nextQuestion);
 answerOne.addEventListener("click", click.bind(null, answerOne));
